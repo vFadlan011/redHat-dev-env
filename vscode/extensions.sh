7cab7734dbdb5ce6ed66843e9f62e251283bf1b9
Fadlan Abduh <84181402+vFadlan011@users.noreply.github.com>
2024-02-18 10:02:14 +0700

    Update extensions.sh

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -1,10 +1,14 @@
+curl https://raw.githubusercontent.com/vFadlan011/redHat-dev-env/main/vscode/keybindings.json -o keybindings.json
+curl https://raw.githubusercontent.com/vFadlan011/redHat-dev-env/main/vscode/settings.json -o settings.json
+curl https://download-cdn.jetbrains.com/fonts/JetBrainsMono-2.304.zip -o JetbrainsMono-2.zip
+
 pkglist=(
 	shufo.vscode-blade-formatter
-  glitchbl.laravel-create-view
-  ryannaddy.laravel-artisan
-  onecentlin.laravel5-snippets
-  onecentlin.laravel-blade
-  amiralizadeh9480.laravel-extra-intellisense
+  	glitchbl.laravel-create-view
+  	ryannaddy.laravel-artisan
+  	onecentlin.laravel5-snippets
+  	onecentlin.laravel-blade
+  	amiralizadeh9480.laravel-extra-intellisense
 	bmewburn.vscode-intelephense-client
 	bradlc.vscode-tailwindcss
 	christian-kohler.path-intellisense
